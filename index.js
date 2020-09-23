c840cb795ff83a03fe87bda2b724905d9f521acb
deniztraka <deniz.traka@gmail.com>
2020-09-23 11:30:05 +0300

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import 'express-async-errors';
+import bodyParser from 'body-parser';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+//routes
+app.get("/", (req, res) => {
+    res.send("online");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server is listening");
+});
