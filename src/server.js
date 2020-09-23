774642e29a333a5f6394dd98baca7db94c8e4940
Deniz Traka <d.traka@tr.emakina.com>
2020-09-23 14:44:22 +0300

    testing redis connection

--- src/server.js
+++ src/server.js
@@ -8,6 +8,17 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
+var redis = require('redis');
+var redisClient = redis.createClient(6379,"mycachecluster.6i0cy3.ng.0001.eun1.cache.amazonaws.com");
+
+redisClient.on('connect', function () {
+    console.log('redis client is succesfully connected.');
+});
+ 
+redisClient.on('error', function (err) {
+    console.log('error on redis client connection' + err);
+});
+
 //routes
 app.get("/", (req, res) => {
     res.send("online v1");
