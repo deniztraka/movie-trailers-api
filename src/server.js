ddbfde1f1f6a65213ae73da1d7814288d2e6f45f
Deniz Traka <d.traka@tr.emakina.com>
2020-09-23 16:28:04 +0300

    testing env variables

--- src/server.js
+++ src/server.js
@@ -9,12 +9,13 @@ const app = express();
 app.use(bodyParser.json());
 
 var redis = require('redis');
-var redisClient = redis.createClient(6379,"mycachecluster.6i0cy3.ng.0001.eun1.cache.amazonaws.com");
+
+var redisClient = redis.createClient(6379, process.env.REDIS_CLUSTER_HOST);
 
 redisClient.on('connect', function () {
     console.log('redis client is succesfully connected.');
 });
- 
+
 redisClient.on('error', function (err) {
     console.log('error on redis client connection' + err);
 });
